8c200177b469eba941bb13bab7c9c8442bf837e0
chohangmin <slightsmille@gmail.com>
2024-08-20 09:54:03 +0900

    Finish 2 step, Implement beamer

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
-import 'package:clone_radish_app/home_screen.dart';
-import 'package:clone_radish_app/splash_screen.dart';
+import 'package:beamer/beamer.dart';
+import 'package:clone_radish_app/router/locations.dart';
+import 'package:clone_radish_app/screens/home_screen.dart';
+import 'package:clone_radish_app/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
+
+final _routerDelegate = BeamerDelegate(
+    locationBuilder:
+        BeamerLocationBuilder(beamLocations: [HomeLocation()]).call);
 
 void main() {
   runApp(const MyApp());
@@ -22,12 +28,25 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class RadishApp extends StatelessWidget {
+  const RadishApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      routeInformationParser: BeamerParser(),
+      routerDelegate: _routerDelegate,
+    );
+  }
+}
+
 StatelessWidget _splashedLoadingWidget(AsyncSnapshot snapshot) {
   if (snapshot.hasError) {
     print('에러가 발생하였습니다');
     return const Text('error');
   } else if (snapshot.hasData) {
-    return const HomeScreen();
+    return const RadishApp();
   } else {
     return const SplashScreen();
   }
