ee26935ac76747ab918e8a5278632c58794f88e9
hmCho Desktop <slightsmille@gmail.com>
2024-09-20 00:35:34 +0900

    dpgb

--- lib/screens/start/address_service.dart
+++ lib/screens/start/address_service.dart
@@ -1,19 +1,20 @@
+import 'package:clone_radish_app/constants/keys.dart';
 import 'package:clone_radish_app/utils/logger.dart';
 import 'package:dio/dio.dart';
 
 class AddressService {
   void SearchAddressByStr(String text) async {
     final formData = {
-      'key값': 'value값',
-      'key값': 'value값',
-      'key값': 'value값',
-      'key값': 'value값',
-      'key값': 'value값',
-      'key값': 'value값'
+      'apiKey': VWORLD_KEY,
+      'service': 'Road',
+      'query': text,
+      'pageUnit': '30',
     };
 
-    final response =
-        await Dio().get('http://apis.vworld.kr/search', queryParameters: formData).catchError((e) {
+
+    final response = await Dio()
+        .get('http://apis.vworld.kr/search', queryParameters: formData)
+        .catchError((e) {
       logger.e(e.message);
     });
 
