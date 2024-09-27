1402e7615bd94e7d2ca1112b20fbbe13f1d341a7
chohangmin <slightsmille@gmail.com>
2024-09-27 11:15:50 +0900

    Finish 25 step

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-https://m.blog.naver.com/rock11201/222616480798
+https://m.blog.naver.com/rock11201/222617638235
 
 https://www.vworld.kr/dev/v4dv_search2_s001.do
 
--- lib/screens/home/items_page.dart
+++ lib/screens/home/items_page.dart
@@ -8,83 +8,93 @@ class ItemsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-      padding: const EdgeInsets.all(common_bg_padding),
-      itemBuilder: (context, index) {
-        return Row(
-          children: [
-            ExtendedImage.network('https://picsum.photos/100'),
-            const SizedBox(
-              width: common_bg_padding,
-            ),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    '펜타곤 모자',
-                    style: Theme.of(context).textTheme.headlineMedium,
-                  ),
-                  const SizedBox(
-                    height: 7,
-                  ),
-                  Text(
-                    '1시간 전',
-                    style: Theme.of(context).textTheme.headlineSmall,
-                  ),
-                  const SizedBox(
-                    height: 7,
-                  ),
-                  const Text('1억원'),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  const SizedBox(
-                    height: 20,
-                    child: FittedBox(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          Icon(
-                            CupertinoIcons.chat_bubble_2,
-                            color: Colors.grey,
-                          ),
-                          Text(
-                            '31',
-                            style: TextStyle(
+    return LayoutBuilder(builder: (context, constraints) {
+      Size size = MediaQuery.of(context).size;
+      final imgSize = size.width / 4;
+      return ListView.separated(
+        padding: const EdgeInsets.all(common_bg_padding),
+        itemBuilder: (context, index) {
+          return Row(
+            children: [
+              ExtendedImage.network(
+                'https://picsum.photos/100',
+                width: imgSize,
+                height: imgSize,
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              const SizedBox(
+                width: common_bg_padding,
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      '펜타곤 모자',
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                    const SizedBox(
+                      height: 7,
+                    ),
+                    Text(
+                      '1시간 전',
+                      style: Theme.of(context).textTheme.headlineSmall,
+                    ),
+                    const SizedBox(
+                      height: 7,
+                    ),
+                    const Text('1억원'),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const SizedBox(
+                      height: 20,
+                      child: FittedBox(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Icon(
+                              CupertinoIcons.chat_bubble_2,
                               color: Colors.grey,
                             ),
-                          ),
-                          Icon(
-                            CupertinoIcons.heart,
-                            color: Colors.grey,
-                          ),
-                          Text(
-                            '19',
-                            style: TextStyle(
+                            Text(
+                              '31',
+                              style: TextStyle(
+                                color: Colors.grey,
+                              ),
+                            ),
+                            Icon(
+                              CupertinoIcons.heart,
                               color: Colors.grey,
                             ),
-                          ),
-                        ],
+                            Text(
+                              '19',
+                              style: TextStyle(
+                                color: Colors.grey,
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          ],
-        );
-      },
-      separatorBuilder: (context, index) {
-        return const Divider(
-          height: common_sm_padding * 3,
-          thickness: 1,
-          color: Colors.grey,
-          indent: common_sm_padding,
-          endIndent: common_sm_padding,
-        );
-      },
-      itemCount: 10,
-    );
+            ],
+          );
+        },
+        separatorBuilder: (context, index) {
+          return const Divider(
+            height: common_sm_padding * 3,
+            thickness: 1,
+            color: Colors.grey,
+            indent: common_sm_padding,
+            endIndent: common_sm_padding,
+          );
+        },
+        itemCount: 10,
+      );
+    });
   }
 }
